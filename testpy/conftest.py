10ddf6d209672b0c6d3c48a8336fa2833fada134
Kenzo <GitHubKenzo@users.noreply.github.com>
2026-06-02 10:15:33 +0900

    end_work: auto-commit at 2026-06-02 10:15:31

--- testpy/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# testpy/ の親ディレクトリ（プロジェクトルート）を import path に追加
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if ROOT not in sys.path:
-    sys.path.insert(0, ROOT)
